4b4971934d8b0f4930d91078f5e4df2fcb9c7b85
Juha Rantanen <juha@codercoded.com>
2022-10-08 10:04:04 +0300

    keymap: fine tune to better 3x6

--- sofle-rgb/keymap.c
+++ sofle-rgb/keymap.c
@@ -24,152 +24,56 @@
 #define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv,Override)
 
 enum sofle_layers {
-    _DEFAULTS = 0,
-    _QWERTY = 0,
-    _LOWER,
-    _RAISE,
-    _ADJUST
+    _BASE = 0,
+    _SYM,
+    _NAV,
+    _FUN
 };
 
-enum custom_keycodes {
-    KC_LOWER,
-    KC_RAISE,
-    KC_ADJUST
-};
+#define MO_SYM  MO(_SYM)
+
+#define MO_NAV  MO(_NAV)
+
+#define MO_FUN  MO(_FUN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-// The Finnish stardard layouts just for reference here
-/*
- * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
- * │ § │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ + │ ´ │       │
- * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
- * │     │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ Å │ ¨ │     │
- * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
- * │      │ A │ S │ D │ F │ G │ H │ J │ K │ L │ Ö │ Ä │ ' │    │
- * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
- * │    │ < │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ - │          │
- * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
- * │    │    │    │                        │    │    │    │    │
- * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
- */
-
-/* QWERTY
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  +   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  '   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   Ö  |  Ä   |
- * |------+------+------+------+------+------|  Mute |    |  Play |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   -  |RShift|
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Space  /       \Enter \  |RAISE | RCTR | RAlt | RGUI |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            '-----------------------------------'           '------''---------------------------'
- */
-
-[_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    FI_PLUS,
-  KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    FI_QUOT,
-  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    FI_ODIA, FI_ADIA,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,          KC_MPLY, KC_N,    KC_M,    FI_COMM, FI_DOT,  FI_MINS, KC_RSFT,
-                 KC_LGUI,KC_LALT,KC_LCTRL, KC_LOWER, KC_SPC,           KC_ENT,  KC_RAISE,KC_RCTRL,KC_RALT, KC_RGUI
+[_BASE] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    FI_QUOT,
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    FI_ODIA, FI_ADIA,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_MUTE,        KC_MPLY, KC_N,    KC_M,    FI_COMM, FI_DOT,  FI_MINS, KC_RSFT,
+                    KC_LGUI, KC_LALT, KC_LCTL, MO_SYM, KC_SPC,         KC_ENT,  MO_NAV,  KC_RCTL, KC_RALT, KC_RGUI
 ),
 
-/* Shifted symbols
- * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
- * │ ½ │ ! │ " │ # │ ¤ │ % │ & │ / │ ( │ ) │ = │ ? │ ` │       │
- * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
- * │     │   │   │   │   │   │   │   │   │   │   │   │ ^ │     │
- * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
- * │      │   │   │   │   │   │   │   │   │   │   │   │ * │    │
- * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
- * │    │ > │   │   │   │   │   │   │   │ ; │ : │ _ │          │
- * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
- * │    │    │    │                        │    │    │    │    │
- * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
- */
-
-/* AltGr symbols
- * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
- * │   │   │ @ │ £ │ $ │ € │   │ { │ [ │ ] │ } │ \ │   │       │
- * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
- * │     │   │   │   │   │   │   │   │   │   │   │   │ ~ │     │
- * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
- * │      │   │   │   │   │   │   │   │   │   │   │   │   │    │
- * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
- * │    │ | │   │   │   │   │   │   │ µ │   │   │   │          │
- * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
- * │    │    │    │                        │    │    │    │    │
- * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
- */
-
-/* LOWER
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   !  |   "  |   &  |   =  |   +  |-------.    ,-------|   /  |   {  |   (  |   )  |   }  |   ?  |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |   @  |   |  |   ~  |   -  |-------|    |-------|   \  |   <  │   [  │   ]  │   >  │      |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |      |      | /       /       \ Tab  \  |      |      |      |      |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            '-----------------------------------'           '------''---------------------------'
- */
-[_LOWER] = LAYOUT(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                           KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
-  _______, FI_EXLM, FI_DQUO, FI_AMPR, FI_EQL,  FI_PLUS,                         FI_SLSH, FI_LCBR, FI_LPRN, FI_RPRN, FI_RCBR, FI_QUES,
-  _______, XXXXXXX, FI_AT,   FI_PIPE, FI_TILD, FI_MINS, _______,       _______, FI_BSLS, FI_LABK, FI_LBRC, FI_RBRC, FI_RABK, _______,
+[_SYM] = LAYOUT(
+  _______, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
+  _______, XXXXXXX, FI_PERC, FI_DLR,  FI_EURO, XXXXXXX,                         XXXXXXX, FI_TILD, FI_CIRC, FI_DQUO,  FI_GRV, FI_QUOT,
+  _______, FI_LABK, FI_LCBR, FI_LBRC, FI_LPRN, FI_BSLS,                         FI_SLSH, FI_RPRN, FI_RBRC, FI_RCBR, FI_RABK, XXXXXXX,
+  _______, FI_EXLM, FI_AT,   FI_HASH, FI_AMPR, FI_PIPE, _______,       _______, FI_ASTR, FI_EQL,  FI_PLUS, FI_MINS, FI_QUES, _______,
                     _______, _______, _______, _______, _______,        KC_TAB, _______, _______, _______, _______
 ),
 
-/* RAISE
- * ,----------------------------------------.                     ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |   €  |      |      |                    | PgUp | Ins  |  Up  | Del  | Pscr |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   *  |   `  |   $  |   #  |      |-------.    ,-------| PgDn | Left | Down | Right| BSpc |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |   %  |   ^  |      |-------|    |-------| App  | Home |      | End  |      |      |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |      |      | / BSpc  /       \      \  |      |      |      |      |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            '-----------------------------------'           '------''---------------------------'
- */
-[_RAISE] = LAYOUT(
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, FI_EURO, XXXXXXX, XXXXXXX,                         KC_PGUP, KC_INS,  KC_UP,   KC_DEL,  KC_PSCR, XXXXXXX,
-  _______, FI_ASTR, FI_GRV,  FI_DLR,  FI_HASH, XXXXXXX,                         KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, FI_PERC, FI_CIRC, XXXXXXX, _______,       _______, KC_APP,  KC_HOME, XXXXXXX, KC_END,  XXXXXXX, _______,
+[_NAV] = LAYOUT(
+  _______, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
+  _______, FI_COMM, FI_7,    FI_8,    FI_9,    FI_MINS,                         KC_PGUP, XXXXXXX, KC_UP,   KC_DEL,  KC_PSCR, XXXXXXX,
+  _______, FI_DOT,  FI_4,    FI_5,    FI_6,    FI_PLUS,                         KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
+  _______, FI_0,    FI_1,    FI_2,    FI_3,    FI_EQL,  _______,       _______, KC_INS,  KC_HOME, XXXXXXX, KC_END,  XXXXXXX, _______,
                     _______, _______, _______, _______, KC_BSPC,       _______, _______, _______, _______, _______
 ),
-/* ADJUST
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Reset|      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      | Prev | Play | Next |      |-------.    ,-------|      |      |      |      |      |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |      |      | /       /       \      \  |      |      |      |      |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            '-----------------------------------'           '------''---------------------------'
- */
-  [_ADJUST] = LAYOUT(
+
+[_FUN] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
-  RESET,   _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
-  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,                         _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, KC_VOLD, KC_MUTE, KC_VOLU, _______,                         _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,                         _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______,
+  _______, _______, _______, _______, _______, _______, _______,       _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F12,  _______,
                     _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
   )
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+   return update_tri_layer_state(state, _SYM, _NAV, _FUN);
+}
 
 #ifdef OLED_ENABLE
 
@@ -185,52 +89,39 @@ static void render_logo(void) {
 }
 
 
-static void print_status_narrow(void) {
-    // Print current mode
-    oled_write_P(PSTR("\n\n"), false);
-    oled_write_ln_P(PSTR("Dane\nEvans"), false);
-
-    oled_write_ln_P(PSTR(""), false);
-
-    switch (get_highest_layer(default_layer_state)) {
-        case _QWERTY:
-            oled_write_ln_P(PSTR("Qwert\n"), false);
-            break;
-
-        default:
-            oled_write_ln_P(PSTR("Undef\n"), false);
-    }
-    oled_write_P(PSTR("\n"), false);
-    // Print current layer
-    oled_write_ln_P(PSTR("LAYER"), false);
-    switch (get_highest_layer(layer_state)) {
-        case _QWERTY:
-            oled_write_P(PSTR("Base\n"), false);
-            break;
-        case _RAISE:
-            oled_write_P(PSTR("Raise\n"), false);
-            break;
-        case _LOWER:
-            oled_write_P(PSTR("Lower\n"), false);
-            break;
-        case _ADJUST:
-            oled_write_P(PSTR("Adj\n"), false);
-            break;
-        default:
-            oled_write_ln_P(PSTR("Undef\n"), false);
-    }
-}
+// static void render_status(void) {
+//     // Print current layer
+//     oled_write_ln_P(PSTR("LAYER"), false);
+//     switch (get_highest_layer(layer_state)) {
+//         case _BASE:
+//             oled_write_P(PSTR("BASE\n"), false);
+//             break;
+//         case _NAV:
+//             oled_write_P(PSTR("NAV\n"), false);
+//             break;
+//         case _SYM:
+//             oled_write_P(PSTR("SYM\n"), false);
+//             break;
+//         case _FUN:
+//             oled_write_P(PSTR("FUN\n"), false);
+//             break;
+//         default:
+//             oled_write_ln_P(PSTR("???\n"), false);
+//     }
+// }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
+        // return OLED_ROTATION_270;
+        return OLED_ROTATION_180;
     }
     return rotation;
 }
 
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
-        print_status_narrow();
+        // render_status();
+        render_logo();
     } else {
         render_logo();
     }
@@ -239,87 +130,20 @@ bool oled_task_user(void) {
 
 #endif
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case KC_LOWER:
-            if (record->event.pressed) {
-                layer_on(_LOWER);
-            } else {
-                layer_off(_LOWER);
-            }
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            return false;
-        case KC_RAISE:
-            if (record->event.pressed) {
-                layer_on(_RAISE);
-            } else {
-                layer_off(_RAISE);
-            }
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            return false;
-        case KC_ADJUST:
-            if (record->event.pressed) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
-            }
-            return false;
-    }
-    return true;
-}
-
 #ifdef ENCODER_ENABLE
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
-        switch (get_highest_layer(layer_state)) {
-            case _QWERTY:
-                if (clockwise) {
-                tap_code(KC_VOLD);
-                } else {
-                tap_code(KC_VOLU);
-                }
-            break;
-            case _RAISE:
-            case _LOWER:
-                if (clockwise) {
-                tap_code(KC_UP);
-                } else {
-                tap_code(KC_DOWN);
-                }
-            break;
-            default:
-                if (clockwise) {
-                tap_code(KC_VOLD);
-                } else {
-                tap_code(KC_VOLU);
-                }
-            break;
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     } else if (index == 1) {
-        switch (get_highest_layer(layer_state)) {
-            case _QWERTY:
-                if (clockwise) {
-                tap_code(KC_WH_U);
-                } else {
-                tap_code(KC_WH_D);
-                }
-            break;
-            case _RAISE:
-            case _LOWER:
-                if (clockwise) {
-                tap_code(KC_MPRV);
-                } else {
-                tap_code(KC_MNXT);
-                }
-            break;
-            default:
-                if (clockwise) {
-                tap_code(KC_WH_U);
-                } else {
-                tap_code(KC_WH_D);
-                }
-            break;
+        if (clockwise) {
+            tap_code(KC_MPRV);
+        } else {
+            tap_code(KC_MNXT);
         }
     }
     return true;
